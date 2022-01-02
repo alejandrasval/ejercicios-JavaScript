161e9d702c62339676028672cad8a011f2b61d0b
Alejandra Sánchez <ing.alejandrasanchezv@gmail.com>
2022-01-01 21:46:10 -0500

    update

--- EjerciciosJavaScript.js
+++ EjerciciosJavaScript.js
@@ -507,23 +507,226 @@ average([1, 4, 9, 2, 5, 6, 7, 8, 3]);
 
 La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
   - Todos los datos del objeto son obligatorios.
-  - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
-     7 restantes números.
+  - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 7 restantes números.
   - Valida que el título no rebase los 100 caracteres.
   - Valida que el director no rebase los 50 caracteres.
   - Valida que el año de estreno sea un número entero de 4 dígitos.
   - Valida que el país o paises sea introducidos en forma de arreglo.
   - Valida que los géneros sean introducidos en forma de arreglo.
-  - Valida que los géneros introducidos esten dentro de los géneros 
-     aceptados*.
+  - Valida que los géneros introducidos estén dentro de los géneros aceptados*.
   - Crea un método estático que devuelva los géneros aceptados*.
-  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
-    decimal de una posición.
+  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser decimal de una posición.
   - Crea un método que devuelva toda la ficha técnica de la película.
-  - Apartir de un arreglo con la información de 3 películas genera 3 
-    instancias de la clase de forma automatizada e imprime la ficha técnica 
-    de cada película.
+  - Apartir de un arreglo con la información de 3 películas genera 3 instancias de la clase de forma automatizada e imprime la ficha técnica de cada película.
 
 * Géneros Aceptados: Action, Adult, Adventure, Animation, Biography, Comedy, Crime, Documentary ,Drama, Family, Fantasy, Film Noir, Game-Show, History, Horror, Musical, Music, Mystery, News, Reality-TV, Romance, Sci-Fi, Short, Sport, Talk-Show, Thriller, War, Western. */
 
-/* */
+class Pelicula {
+  constructor({ id, titulo, director, estreno, pais, generos, calificacion }) {
+    this.id = id;
+    (this.titulo = titulo), (this.director = director);
+    this.estreno = estreno;
+    this.pais = pais;
+    this.generos = generos;
+    this.calificacion = calificacion;
+
+    this.validarIMDB(id);
+    this.validarTitulo(titulo);
+    this.validarDirector(director);
+    this.validarEstreno(estreno);
+    this.validarPais(pais);
+    this.validarGeneros(generos);
+    this.validarCalificacion(calificacion);
+  }
+
+  static get listaGeneros() {
+    return [
+      "Action",
+      "Adult",
+      "Adventure",
+      "Animation",
+      "Biography",
+      "Comedy",
+      "Crime",
+      "Documentary",
+      "Drama",
+      "Family",
+      "Fantasy",
+      "Film Noir",
+      "Game-Show",
+      "History",
+      "Horror",
+      "Musical",
+      "Music",
+      "Mystery",
+      "News",
+      "Reality-TV",
+      "Romance",
+      "Sci-Fi",
+      "Short",
+      "Sport",
+      "Talk-Show",
+      "Thriller",
+      "War",
+      "Western",
+    ];
+  }
+
+  static generosAceptados() {
+    return console.info(
+      `Los géneros aceptados son: ${Pelicula.listaGeneros.join(",")}`
+    );
+  }
+
+  validarCadena(propiedad, valor) {
+    if (!valor) return console.warn(`${propiedad} "${valor}" está vacío`);
+
+    if (typeof valor !== "string")
+      return console.warn(
+        `${propiedad} "${valor}" ingresado, no es una cadena de texto`
+      );
+
+    return true;
+  }
+
+  validarLongitudCadena(propiedad, valor, longitud) {
+    if (valor.length > longitud)
+      return console.error(
+        `${propiedad} "${valor} excede el número de caracteres permitidos ${longitud}"`
+      );
+    return true;
+  }
+
+  validarNumero(propiedad, valor) {
+    if (!valor) return console.warn(`${propiedad} "${valor}" está vacío`);
+
+    if (typeof valor !== "number")
+      return console.warn(`${propiedad} "${valor}" ingresado, no es un número`);
+
+    return true;
+  }
+
+  validarArreglo(propiedad, valor) {
+    if (!valor) return console.warn(`${propiedad} "${valor}" está vacío`);
+
+    if (!(valor instanceof Array))
+      return console.warn(
+        `${propiedad} "${valor}" ingresado, no es un arreglo`
+      );
+
+    if (valor.length === 0)
+      return console.error(`El valor "${valor}" no tiene datos`);
+
+    for (let cadena of valor) {
+      if (typeof cadena !== "string")
+        return console.error(
+          `El valor "${cadena}" ingresado, no es una cadena de texto`
+        );
+    }
+
+    return true;
+  }
+
+  validarIMDB(id) {
+    if (this.validarCadena("IMDB id", id)) {
+      if (!/^([a-z]){2}([0-9]){7}$/.test(id)) {
+        return console.error(
+          `IMBD id "${id}" no es válido, debe tener 9 carácteres, los 2 primeros letras minúsculas y los 7 restantes números`
+        );
+      }
+    }
+  }
+
+  validarTitulo(titulo) {
+    if (this.validarCadena("Título", titulo)) {
+      this.validarLongitudCadena("Título", titulo, 100);
+    }
+  }
+
+  validarDirector(director) {
+    if (this.validarCadena("Director", director)) {
+      this.validarLongitudCadena("Director", director, 50);
+    }
+  }
+
+  validarEstreno(estreno) {
+    if (this.validarNumero("Año de estreno", estreno)) {
+      if (!/^([0-9]){4}$/.test(estreno)) {
+        return console.error(
+          `Año de estreno "${estreno}" no es válido, debe ser un número de 4 dígitos`
+        );
+      }
+    }
+  }
+
+  validarPais(pais) {
+    this.validarArreglo("País", pais);
+  }
+
+  validarGeneros(generos) {
+    if (this.validarArreglo("Géneros", generos)) {
+      for (const genero of generos) {
+        console.log(genero, Pelicula.listaGeneros.includes(genero));
+        if (!Pelicula.listaGeneros.includes(genero)) {
+          console.error(`Géneros incorrectos "${generos.join(",")}"`);
+          Pelicula.generosAceptados();
+        }
+      }
+    }
+  }
+
+  validarCalificacion(calificacion) {
+    if (this.validarNumero("Calificación", calificacion)) {
+      return calificacion < 0 || calificacion > 10
+        ? console.error(
+            `La calificación tiene que estar en un rango entre 0 y 10`
+          )
+        : (this.calificacion = calificacion.toFixed(1));
+    }
+  }
+
+  fichaTecnica() {
+    console.info(
+      `Ficha técnica: \nTítulo: "${this.titulo}"\nDirector: "${
+        this.director
+      }"\nAño: "${this.estreno}"\nPaís: "${this.pais.join(
+        "-"
+      )}"\nGéneros: "${this.generos.join(", ")}"\nCalificación: "${
+        this.calificacion
+      }"\nIMDB id: "${this.id}"`
+    );
+  }
+}
+
+const misPelis = [
+  {
+    id: "tt0111161",
+    titulo: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    estreno: 1994,
+    pais: ["United States"],
+    generos: ["Drama", "Crime"],
+    calificacion: 9.3
+  },
+  {
+    id: "tt3553976",
+    titulo: "Captain Fantastic",
+    director: "Matt Ross",
+    estreno: 2016,
+    pais: ["United States"],
+    generos: ["Drama", "Comedy"],
+    calificacion: 7.9
+  },
+  {
+    id: "tt0012349",
+    titulo: "The Kid",
+    director: "Charles Chaplin",
+    estreno: 1921,
+    pais: ["United States"],
+    generos: ["Comedy"],
+    calificacion: 8.3
+  }
+
+]
+
+misPelis.forEach(el => new Pelicula(el).fichaTecnica())
